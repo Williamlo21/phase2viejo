5c6c3429cb4f4ed173c8d827585b878f6b69ca19
William lopez <lopezrebellonw@gmail.com>
2024-01-19 11:23:11 -0500

    hemos hecho el resaltado de la barra de navegacion y el estilo del aside

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -37,32 +37,46 @@
   5: Administrativo
   6: visitante
   8: Super Admin  -->
-  <!-- Seleccion de opciones que se muestran en la barra de navegacion de acuerdo al roll -->
+    <!-- Seleccion de opciones que se muestran en la barra de navegacion de acuerdo al roll -->
+    <?php
+    $miPerfilClass = $hacerRegistroClass = $informesClass = $crearVigilanteClass = '';
+
+    switch ($_SESSION['nav']) {
+      case 'miPerfil':
+        $miPerfilClass = 'active';
+        break;
+      case 'hacerRegistro':
+        $hacerRegistroClass = 'active';
+        break;
+      case 'informes':
+        $informesClass = 'active';
+        break;
+      case 'crearVigilante':
+        $crearVigilanteClass = 'active';
+        break;
+        // Agrega más casos según sea necesario
+    }
+    ?>
+
     <nav class="barraNavegacion">
       <ul>
-        <li><a href="<?= base_url ?>usuarios/miPerfil">Mi perfil</a></li>
+        <li class="<?= $miPerfilClass ?>"><a href="<?= base_url ?>usuarios/miPerfil">Mi perfil</a></li>
 
-        <?php
-        if ($_SESSION['identity']->id_roll == 2 || $_SESSION['identity']->id_roll == 4 || $_SESSION['identity']->id_roll == 8) {
-        ?>
-          <li><a href="<?= base_url ?>usuarios/hacerRegistro">Hacer registro</a></li>
-          <li><a href="<?= base_url ?>usuarios/informes">Informes</a></li>
-        <?php
-        }
-        if ($_SESSION['identity']->id_roll == 2 || $_SESSION['identity']->id_roll == 8) {
-        ?>
-          <li><a href="<?= base_url ?>usuarios/crearVigilante">Crear vigilante</a></li>
-        <?php
-        }
-        ?>
-
-
-        <?php if (isset($_SESSION['identity'])) { ?>
-          <li><a href="<?= base_url ?>usuarios/logout" class="cerrar-sesion">cerrar sesión</a></li>
+        <?php if ($_SESSION['identity']->id_roll == 2 || $_SESSION['identity']->id_roll == 4 || $_SESSION['identity']->id_roll == 8) { ?>
+          <li class="<?= $hacerRegistroClass ?>"><a href="<?= base_url ?>usuarios/hacerRegistro">Hacer registro</a></li>
+          <li class="<?= $informesClass ?>"><a href="<?= base_url ?>usuarios/informes">Informes</a></li>
         <?php } ?>
 
+        <?php if ($_SESSION['identity']->id_roll == 2 || $_SESSION['identity']->id_roll == 8) { ?>
+          <li class="<?= $crearVigilanteClass ?>"><a href="<?= base_url ?>usuarios/crearVigilante">Crear vigilante</a></li>
+        <?php } ?>
+
+        <?php if (isset($_SESSION['identity'])) { ?>
+          <li><a href="<?= base_url ?>usuarios/logout" class="cerrar-sesion">Cerrar sesión</a></li>
+        <?php } ?>
       </ul>
     </nav>
+
 
   <?php
   } else {
